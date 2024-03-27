c78b82ad405facfd5c5447bc18f0f0504eb530ec
666TSL666 <163123757+666TSL666@users.noreply.github.com>
2024-03-27 14:36:11 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 'apple', 'banana')
+print(immutable_var)
+#immutable_var [0] = 4 кортежи не поддерживают обащение по элементам
+print (immutable_var)
+mutable_list = [1, 2, 'a', 'b', 'coconut']
+print (mutable_list)
